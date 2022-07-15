3be74077831fbaf3816f56f436aa66a50b252e0d
Chidi <chidex5@gmail.com>
2022-07-15 05:08:57 +0100

    Setup eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 function App() {
-	return (
-		<div className="App">
-			<header className="App-header">Transactions</header>
-		</div>
-	);
+  return (
+    <div className="App">
+      <header className="App-header">Transactions</header>
+    </div>
+  );
 }
 
 export default App;
