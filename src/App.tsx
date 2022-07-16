cd0badda6c0029d4648e64a3cc230c9485f8329c
Chidi <chidex5@gmail.com>
2022-07-16 07:09:22 +0100

    Manage JSONdata state with React query

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import { useQuery } from 'react-query';
 import { generateJSON } from './api/generateJSON';
 
 function App() {
-  generateJSON();
+  const { isLoading, isError, data } = useQuery('expenses', generateJSON);
+
+  if (isLoading) {
+    return <h2>Loading ...</h2>;
+  }
+
+  if (isError) {
+    return <h2>Something went wrong, can not load JSON data</h2>;
+  }
+
+  console.log(data);
+
   return (
     <div className="App">
       <header className="App-header">Transactions</header>
--- src/api/generateJSON.ts
+++ src/api/generateJSON.ts
@@ -1,14 +1,12 @@
 import csv from 'csvtojson';
 
 export const generateJSON = async () => {
+  let jsonData: jsonDataObject[] = [];
   const response = await fetch(
     'https://raw.githubusercontent.com/Pectus-Finance/hiring-exercises/master/frontend/expanses.csv',
   );
 
   const data = await response.text();
-  csv()
-    .fromString(data)
-    .then((jsonObj: jsonDataObject[]) => {
-      console.log(jsonObj);
-    });
+  jsonData = await csv().fromString(data);
+  return jsonData;
 };
