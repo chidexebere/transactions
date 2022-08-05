06a951323f10c572007f844ad6832c0c8f9c008f
Chidi <chidex5@gmail.com>
2022-08-05 17:39:13 +0100

    Add some unit and integration tests

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,11 +1,11 @@
-import { waitForElementToBeRemoved } from '@testing-library/react';
+import { fireEvent, waitForElementToBeRemoved } from '@testing-library/react';
 import { rest } from 'msw';
 import App from '../App';
 import { server } from '../setupTests';
 import { renderWithClient } from './utils';
 
 describe('<App />', () => {
-  it('should fetch data display on table after displaying loading', async () => {
+  it('should fetch data display on expense table after displaying loading', async () => {
     const result = renderWithClient(<App />);
 
     // Wait For Loading
@@ -14,11 +14,65 @@ describe('<App />', () => {
       { timeout: 2000 },
     );
 
+    // Display Expense data
     expect(
       result.getByRole('button', { name: 'View Grouped Data' }),
     ).toBeInTheDocument();
     expect(result.getByRole('table', { name: 'Expenses' })).toBeInTheDocument();
     expect(result.getAllByText(/IT/i)[0]).toBeInTheDocument();
+  });
+
+  it('should display select component and group table after View Grouped Data button has been clicked', async () => {
+    const result = renderWithClient(<App />);
+
+    // Wait For Loading
+    await waitForElementToBeRemoved(
+      () => result.getByRole('alert', { name: 'loading' }),
+      { timeout: 2000 },
+    );
+
+    // Display Group Expense data after clicking on View Grouped Data button
+    fireEvent.click(result.getByRole('button', { name: 'View Grouped Data' }));
+
+    expect(result.getByLabelText('Total Expenses by:')).toBeInTheDocument();
+    expect(
+      result.getByRole('table', { name: 'Grouped Expenses' }),
+    ).toBeInTheDocument();
+    expect(result.getAllByText(/IT/i)[0]).toBeInTheDocument();
+  });
+
+  it('should change the header of the table with option selected from the select component', async () => {
+    const result = renderWithClient(<App />);
+
+    // Wait For Loading
+    await waitForElementToBeRemoved(
+      () => result.getByRole('alert', { name: 'loading' }),
+      { timeout: 2000 },
+    );
+
+    fireEvent.click(result.getByRole('button', { name: 'View Grouped Data' }));
+
+    const select = result.getByLabelText('Total Expenses by:');
+    fireEvent.change(select, { target: { value: 'Date' } });
+    expect(result.getByText(/Date/i)).toBeInTheDocument();
+  });
+
+  it('should display each table once respective buttons are clicked', async () => {
+    const result = renderWithClient(<App />);
+
+    // Wait For Loading
+    await waitForElementToBeRemoved(
+      () => result.getByRole('alert', { name: 'loading' }),
+      { timeout: 2000 },
+    );
+
+    fireEvent.click(result.getByRole('button', { name: 'View Grouped Data' }));
+    expect(
+      result.getByRole('table', { name: 'Grouped Expenses' }),
+    ).toBeInTheDocument();
+
+    fireEvent.click(result.getByRole('button', { name: 'Back to Expenses' }));
+    expect(result.getByRole('table', { name: 'Expenses' })).toBeInTheDocument();
   });
 
   it('should display error if fetching JSON data fails ', async () => {
